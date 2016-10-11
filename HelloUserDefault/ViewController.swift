1a56ed2021db82e232a0020ba5b84b87dedaec56
TeChengHung <dan12411@gmail.com>
2016-10-11 19:58:22 +0800

    First Ci

--- HelloUserDefault/ViewController.swift
+++ HelloUserDefault/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 1. Generate a Array
+    //    var toDo = ["buy milk", "iOS test", "sleep", "make money"]
+    
+    //////////////////////////////////////
+    /// 如果怕拼錯Key 用static 變數!!!!!! ///
+    static let myKey = "todo"          ///
+    //////////////////////////////////////
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 2. Save Array Data
+        //        UserDefaults.standard.set(toDo, forKey: "todo")
+        
+        //        UserDefaults.standard.synchronize()
+        
+        // 3. Get Array Data
+        if let loadedToDo = UserDefaults.standard.object(forKey: "todo") as? [String] {
+            print(loadedToDo)
+            
+        }
+        
+        /// 如果資料是數字，也可以這樣做
+        UserDefaults.standard.set(87, forKey: "yeah")
+        UserDefaults.standard.synchronize()
+        
+        UserDefaults.standard.integer(forKey: "yeah")
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    
 }
 
